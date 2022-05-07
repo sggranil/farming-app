7dbc350443c36e013019149f39d3a8b7075ba423
Simoun <dev.simongranil@gmail.com>
2022-05-07 09:35:43 +0800

    new app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { EquipmentsComponent } from './home/equipments/equipments.component';
+import { RequestComponent } from './home/request/request.component';
+import { InventoryComponent } from './home/inventory/inventory.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'equipments', pathMatch: 'full' },
+
+  { path: '', component: HomeComponent, children: [
+    { path: 'equipments', component: EquipmentsComponent },
+    { path: 'inventory', component: InventoryComponent },
+    { path: 'request', component: RequestComponent}
+
+  ]},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MaterialModules } from './material.module';
+
+import { HomeComponent } from './home/home.component';
+import { EquipmentsComponent } from './home/equipments/equipments.component';
+import { InventoryComponent } from './home/inventory/inventory.component';
+import { RequestComponent } from './home/request/request.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    EquipmentsComponent,
+    InventoryComponent,
+    RequestComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModules
   ],
   providers: [],
   bootstrap: [AppComponent]
